b473a7ae03bd9d0e6e4edf8d36bca31f712a9bf3
Mehmed Jusic <>
2018-04-11 13:37:14 +0200

    Added Custom RnD component

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Rnd from 'react-rnd';
 import InfoGraphicComponent from './components/InfoGraphicComponent';
 import ResizableBox from 'react-resizable-component';
 import './App.css';
 import img1 from './img/1.jpg';
 import img2 from './img/2.jpg';
 import logo from './img/logo.png';
-
-const style = {
-  display: "flex",
-  border: "solid 1px #ddd",
-  background: "#f0f0f0"
-};
+import RndCustom from './components/RnDComponent';
 
 export default class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      width: 100,
-      height: 100,
-      x: 10,
-      y: 45
     }
   }
 
@@ -54,39 +44,9 @@ export default class App extends React.Component {
                 Templates
               </div>
               <div className="box">
-                <p>Albums</p>
-                <Rnd
-          style={style}
-          size={{ width: this.state.width, height: this.state.height }}
-          position={{ x: this.state.x, y: this.state.y }}
-          onDragStop={(e, d) => {
-            this.setState({ x: d.x, y: d.y });
-          }}
-          onResize={(e, direction, ref, delta, position) => {
-            this.setState({
-              width: ref.offsetWidth,
-              height: ref.offsetHeight,
-              ...position
-            });
-          }}
-        >
-          <img
-            style={{ width: this.state.width, height: this.state.height }}
-            src={
-              "https://img.wikinut.com/img/gycf69_-6rv_5fol/jpeg/0/Best-Friends-Img-Src%3AImage%3A-FreeDigitalPhotos.net.jpeg"
-            }
-          />
-        </Rnd>
-                {/* <Rnd         style={style}
-    size={{         width: this.state.width2,         height: this.state.height2
-         }} position={{         x: this.state.x2,         y: this.state.y2
-    }}   onDragStop={(e, d) => {         this.setState({x2: d.x, y2: d.y})
-    }}        onResize={(e, direction, ref, delta, position) => { this.setState({
-              width: ref.offsetWidth,           height: ref.offsetHeight,
-      ...position         });       }}>         <img        src={img2}
-    alt="img2"           className="img" style={{           width:
-    this.state.width,           height: this.state.height         }}/>
-    </Rnd> */}
+               Albums
+              
+               {/* <RndCustom width={500} height={400} text="mehmed" img="http://res.cloudinary.com/demo/image/upload/multiple/folders/sample.jpg"/> */}
               </div>
               <div className="box">
                 InfoGraphics
@@ -100,9 +60,14 @@ export default class App extends React.Component {
             </div>
           </div>
           <div className="main-content-wrapper">
-            <div class="row">
+          <div>
+          <RndCustom width={150} height={150} text="The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It is the second largest extant species after the tiger. It exhibits a pronounced sexual dimorphism; males are larger than females with a typical weight range of 150 to 250 kg (331 to 551 lb) for the former and 120 to 182 kg (265 to 401 lb) for the latter. In addition, male lions have a prominent mane, which is perhaps the most recognisable feature of the species. Both sexes have hairy tufts at the end of their tails." img={img1}/>
+          {/* <RndCustom width={150} height={150} text="Bears are carnivoran mammals of the family Ursidae. They are classified as caniforms, or doglike carnivorans. Although only eight species of bears are extant, they are widespread, appearing in a wide variety of habitats throughout the Northern Hemisphere and partially in the Southern Hemisphere. Bears are found on the continents of North America, South America, Europe, and Asia. Common characteristics of modern bears include large bodies with stocky legs, long snouts, small rounded ears, shaggy hair, plantigrade paws with five nonretractile claws, and short tails." img={img2}/> */}
+
+          </div>
+            <div className="row">
               <div className="content-buttons">
-              <button id="submit" name="button" value="register" className="btn btn-primary">Save as draft</button>
+                <button id="submit" name="button" value="register" className="btn btn-primary">Save as draft</button>
                 <button id="submit" name="button" value="register" className="btn btn-primary">Publish</button>
               </div>
             </div>
